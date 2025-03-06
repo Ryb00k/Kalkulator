ee9f5e85562eb43dcd310f80f7115a6dcfbac5d0
Rybook <m.manel@zset.leszno.pl>
2025-03-06 19:58:30 +0100

    Dodaj pliki projektów.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 2;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 3;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 4;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 5;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 6;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 7;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 8;
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 9;
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 0;
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '*';
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '-';
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '+';
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '/';
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            string equasion = textBox1.Text;
+            List<int> numbers = new List<int>();
+            List<char> operators = new List<char>();
+
+
+            string current_number = "";
+
+            for (int i = 0; i < equasion.Length; i++) {
+
+
+                if (equasion[i] != '+' && equasion[i] != '-' && equasion[i] != '*' && equasion[i] != '/')
+                {
+                    current_number += equasion[i];
+                }
+
+                else {
+
+                    if (!string.IsNullOrEmpty(current_number)) 
+                    {
+                        numbers.Add(int.Parse(current_number));
+                        current_number = ""; 
+                    }
+                    operators.Add(equasion[i]);
+                }
+            }
+            if (!string.IsNullOrEmpty(current_number)) 
+            {
+                numbers.Add(int.Parse(current_number));
+            }
+
+            int result = numbers[0];
+            for (int i = 0; i < operators.Count; i++)
+            {
+                switch (operators[i])
+                {
+                    case '+':
+                        result += numbers[i + 1];
+                        break;
+                    case '-':
+                        result -= numbers[i + 1];
+                        break;
+                    case '*':
+                        result *= numbers[i + 1];
+                        break;
+                    case '/':
+                        if (numbers[i + 1] != 0)
+                            result /= numbers[i + 1];
+                        else
+                            MessageBox.Show("Error");
+                        break;
+                }
+            }
+
+            
+            textBox1.Text = result.ToString();
+
+        }
+    }
+}
